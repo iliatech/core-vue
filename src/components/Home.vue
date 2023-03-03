cff4876589d11b27b7f203deb00c3c01d73cd5bb
iliatech <iadomyshev@gmail.com>
2023-03-03 23:50:27 +0400

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div
+      v-for="tile in tiles"
+      :key="tile.title"
+      class="hello__tile"
+      :style="{ background: tile.color }"
+    >
+      {{ tile.title }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { tilesMock } from "@/mockData/tiles";
+import { ref } from "vue";
+
+const tiles = ref(tilesMock);
+console.log("home vue");
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/fonts.scss";
+.hello {
+  &__tile {
+    margin: 10px;
+    width: 200px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px #eee solid;
+    cursor: pointer;
+    @include font-l;
+  }
+}
+</style>
---
+++ src/mockData/tiles.ts
@@ -0,0 +1,10 @@
+export const tilesMock = [
+  {
+    title: "Food",
+    color: "#88B04B",
+  },
+  {
+    title: "Courses",
+    color: "#FF6F61",
+  },
+];
