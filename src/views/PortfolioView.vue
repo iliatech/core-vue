a3a62a3ce3fa97a25ea9b6060ac5d92a1e4a9798
iliajs <iadomyshev@gmail.com>
2024-09-01 16:18:01 +0200

    sp-27: portfolio left part

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -5,9 +5,10 @@
         <div class="cv__person">ILIA<br />DOMYSHEV</div>
         <div class="cv__position">FRONTEND VUE DEVELOPER</div>
         <div class="cv__contacts">
-          Phone: +34 603 14 99 56<br />
-          Email: iadomyshev@gmail.com<br />
-          GitHub: idomyshev
+          <a href="https://www.linkedin.com/in/idomyshev/">LinkedIn</a>
+          <br />
+          <a href="https://github.com/idomyshev">GitHub</a>
+          <br />
         </div>
         <div
           v-for="section in leftSections"
@@ -18,7 +19,13 @@
             {{ section.title }}
           </div>
           <div class="cv__section-body">
-            {{ section.body }}
+            <div
+              v-for="bodyItem in section.body"
+              :key="bodyItem"
+              class="cv__section-body-item"
+            >
+              {{ bodyItem }}
+            </div>
           </div>
         </div>
       </div>
@@ -29,12 +36,40 @@
 <script lang="ts" setup>
 const leftSections = [
   {
-    title: "TOOLS",
-    body: "...",
+    title: "Frameworks",
+    body: ["Vue 3, Vue 2"],
   },
   {
-    title: "MOBILE TOOLS",
-    body: "...",
+    title: "Languages",
+    body: ["JavaScript, TypeScript, HTML5, CSS3, SASS"],
+  },
+  {
+    title: "Libraries",
+    body: [
+      "PrimeVue, Vuetify, Pinia, Vuex, AWS Amplify, Facebook JS SDK, AmCharts, Mapbox, Deck.gl, Lodash, DateFNS, Axios, html2pdf.js",
+    ],
+  },
+  {
+    title: "Tools",
+    body: [
+      "Git, GitHub",
+      "Docker, GitHub Actions",
+      "Swagger, Postman, Ngrok, Valet, Heroku, npm, nvm",
+      "Webstorm, Visual Studio Code",
+      "Jira, Confluence, Discord, Slack, Asana, Figma",
+      "MacOS, Linux, Windows",
+    ],
+  },
+  {
+    title: "Other technologies",
+    body: [
+      "Frontend: Nuxt, React, Redux + Saga",
+      "Backend: Nest, Express.js, Node.js, C#, ASP.NET, PHP, Laravel, Databases: Prisma, Sequelize, Postgres, MySQL, MSSQL, MongoDB",
+      "Services: Apache, Nginx, IPTables, PowerShell, TFS Deploy Sever, IIS",
+      "OS: Windows Servers, Linux (Debian, Ubuntu) Servers",
+      "Virtualization: VMWare, HyperV",
+      "IDE: Visual Studio",
+    ],
   },
 ];
 </script>
@@ -63,22 +98,37 @@ const leftSections = [
   }
 
   &__person {
-    font-size: 1.75rem;
-    line-height: 2.5rem;
+    font-size: 1.5rem;
+    line-height: 2rem;
   }
 
   &__position {
     margin-top: $px-10;
-    font-size: 1rem;
+    font-size: 1.125rem;
   }
   &__contacts {
     margin-top: $px-10;
     margin-bottom: $px-10;
     font-size: 0.875rem;
+    line-height: 1.25rem;
   }
 
   &__section {
     //
+  }
+
+  &__section-title {
+    font-size: 1rem;
+    text-transform: uppercase;
+  }
+
+  &__section-body {
+    border-top: 1px solid #666;
+    margin-top: $px-5;
+    padding-top: $px-5;
+    margin-bottom: $px-20;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
   }
 }
 </style>
